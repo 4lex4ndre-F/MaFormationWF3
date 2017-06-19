cd80e548148885fc6bbac7ca37a4a3195edb7aea
4lex4andre-F <4lex.fernandes@gmail.com>
2017-06-19 21:55:55 +0200

    oops

---
+++ php/entrainement.php
@@ -0,0 +1,5 @@
+<?php
+echo '<h1>Bonjour</h1>';
+echo 'Nous  sommes  <strong>vendredi</strong>  et  il  fait  beau  !<br>';
+echo '<div class="mazone">Je  crée  une  zone  html  dans  du  php
+    </div>';
